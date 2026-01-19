f0595c61f0e509a2b4674a56228d183826554dc9
Dishankcodes <dishankprajapati420@gmail.com>
2026-01-19 22:59:58 +0530

    make a sidebar design

--- src/main/java/com/example/demo/Controllers/LoginController.java
+++ src/main/java/com/example/demo/Controllers/LoginController.java
@@ -2,6 +2,8 @@ package com.example.demo.Controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class LoginController {
@@ -10,6 +12,27 @@ public class LoginController {
 	public String student_login(){
 		return "student-login";
 	}
+	
+	//Register Method;
+	
+	 @PostMapping("/student-login")
+	 public String registerStudent(
+	            @RequestParam String fullName,
+	            @RequestParam String college,
+	            @RequestParam String email,
+	            @RequestParam String password) {
+
+	        return "student-login";
+	    }
+	 
+	@PostMapping("/student-dasboard")
+	public String loginStudent(@RequestParam String email,
+			@RequestParam String password)
+	{
+		
+		return "student-dashboard";
+	}
+	
 	
 	@GetMapping("/teacher-auth") 
 	public String teacher_login(){
