aea86045745711dbafdea71c854d3e73b82637b4
Dishankcodes <dishankprajapati420@gmail.com>
2026-01-21 21:46:46 +0530

    add a controller for student

--- src/main/java/com/example/demo/Controllers/StudentController.java
+++ src/main/java/com/example/demo/Controllers/StudentController.java
@@ -14,5 +14,51 @@ public class StudentController {
 		return "student-dashboard";
 	}
 	
+	@GetMapping("/student-course")
+	public String student_course()
+	{
+	
+		return "student-course";
+	}
+	
+	@GetMapping("/student-learning")
+	public String student_learning()
+	{
+	
+		return "student-learning";
+	}
+	
+	@GetMapping("/student-news")
+	public String student_announcements() {
+		return "student_announcements";
+	}
+	
+	@GetMapping("/notes")
+	public String student_stuff() {
+		return "student_stuff";
+	}
+	
+	@GetMapping("/student-chat")
+	public String student_chat() {
+		return "student_chat";
+	}
+	
+	@GetMapping("/student-internships")
+	public String student_internships()
+	{
+		return "student_internships";
+	}
+	
+	@GetMapping("/student-profile")
+	public String student_profile()
+	{
+		return "student_profile";
+	}
+	
+	@GetMapping("/student-instructor-feedback")
+	public String student_instructor_feedback()
+	{
+		return "student_inst_review";
+	}
 	
 }
